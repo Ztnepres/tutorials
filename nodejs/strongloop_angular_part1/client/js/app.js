1951ce636091182eedbe8fe403da3721ac301fc0
Rem Koh De <rc@remkohde.com>
2016-01-07 15:23:03 -0500

    init

---
+++ nodejs/strongloop_angular_part1/client/js/app.js
@@ -0,0 +1,44 @@
+angular.module("app", [
+    "ui.router",
+    "lbServices"
+  ])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('sign-up', {
+      url: '/sign-up',
+      templateUrl: 'views/sign-up-form.html',
+      controller: 'SignUpController'
+    })
+
+    .state('sign-up-success', {
+      url: '/sign-up/success',
+      templateUrl: 'views/sign-up-success.html'
+	})
+
+	.state('login', {
+      url: '/login',
+      templateUrl: 'views/login.html',
+      controller: 'AuthLoginController'
+    })
+    
+    .state('logout', {
+	  url: '/logout',
+	  controller: 'AuthLogoutController'
+    })
+
+    .state('all-videos', {
+      url: '/all-videos',
+      templateUrl: 'views/all-videos.html'
+    })
+		
+    .state('forbidden', {
+      url: '/forbidden',
+      templateUrl: 'views/forbidden.html',
+    })
+      
+  }
+]);
